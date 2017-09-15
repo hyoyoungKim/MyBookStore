cd121128ada9a46939afbb77df303aa52b376e10
11601234 <11601234@TAFT-CL316>
2017-09-15 12:14:36 +0800

    intinal commit

---
+++ Mybookstore/App_Code/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Configuration;
+
+namespace Mybookstore.App_Code
+{
+    public class Helper
+    {
+        public static string GetCon()
+        {
+            return ConfigurationManager.ConnectionStrings["Mycon"].ConnectionString;
+        }
+    }
+}
---
+++ Mybookstore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mybookstore.MvcApplication" Language="C#" %>
